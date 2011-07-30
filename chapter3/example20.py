a91a710ec4adab9c839da989d2c0c8cd8ae4c35b
Oliver Hookins <ohookins@gmail.com>
2011-07-31 00:53:57 +0200

    Add stub example 20.

---
+++ chapter3/example20.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+from opencv import cv
+
+if __name__ == "__main__":
+  print """cvGetModuleInfo doesn't appear to work, but in any case it seems
+  support for IPP is reduced in OpenCV 2.0 and performance gains are minimal
+  in 2.0 or later."""
