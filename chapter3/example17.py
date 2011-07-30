cabb2d687c66a6df1273a9dc8be7fb689068c8f0
Oliver Hookins <ohookins@gmail.com>
2011-07-30 23:54:01 +0200

    Ruled out pickle and marshal as alternatives.

--- chapter3/example17.py
+++ chapter3/example17.py
@@ -5,13 +5,7 @@ from opencv import cv
 if __name__ == "__main__":
   print """
   The FileStorage routines do not work at all in python opencv, so don't bother.
+
+  You also cannot use marshal or pickle to store SwigPyObject objects, so there
+  is no easy way of storing the native data types.
   """
-  #fs = cv.cvOpenFileStorage('cfg.yaml', None, cv.CV_STORAGE_WRITE)
-  #cv.cvWriteInt(fs, 'frame_count', 10)
-  #cv.cvStartWriteStruct(fs, 'frame_size', cv.CV_NODE_SEQ)
-  #cv.cvWriteInt(fs, 0, 320)
-  #cv.cvWriteInt(fs, 0, 200)
-  #cv.cvEndWriteStruct(fs)
-  #cv.cvWrite(fs, 'color_cvt_matrix', cv.cmatrix)
-  #cv.cvReleaseFileStorage(fs)
-  #del fs
